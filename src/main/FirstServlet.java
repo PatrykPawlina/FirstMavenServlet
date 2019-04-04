a95c55c19c91fe2ca34aadc8d1c1f4706ef12767
Patryk077 <p.pawlina@poczta.onet.pl>
2019-04-05 00:11:37 +0200

    Load File

--- src/main/FirstServlet.java
+++ src/main/FirstServlet.java
@@ -2,8 +2,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.OutputStream;
 
 public class FirstServlet extends HttpServlet {
     @Override
@@ -11,12 +13,30 @@ public class FirstServlet extends HttpServlet {
 //        resp.setContentType("text/plain; charset=utf-8");
 //        PrintWriter printWriter = resp.getWriter();
 //        printWriter.println("Hello");
-        resp.setContentType("text/html; charset=utf-8");
-        PrintWriter printWriter = resp.getWriter();
-        printWriter.println("<html><head><title>My Servlet</title></head>");
-        printWriter.println("<body>");
-        printWriter.println("<p>This is simple</p>");
-        printWriter.println("<p>Servlet example.</p>");
-        printWriter.println("</body></html>");
+//        resp.setContentType("text/html; charset=utf-8");
+//        PrintWriter printWriter = resp.getWriter();
+//        printWriter.println("<html><head><title>My Servlet</title></head>");
+//        printWriter.println("<body>");
+//        printWriter.println("<p><h2>This is simple</h></p>");
+//        printWriter.println("<p><h3>Servlet example.</h3></p>");
+//        printWriter.println("</body></html>");
+
+        byte[] buffer = loadFile("C:\\java.png");
+        resp.setContentType("image/jpg");
+        resp.setContentLength(buffer.length);
+        resp.addHeader("Content-Description", "attachment;filename=image.jpg");
+        OutputStream outputStream = resp.getOutputStream();
+        outputStream.write(buffer);
+        outputStream.flush();
+    }
+
+    private byte[] loadFile(String nameFile) throws IOException {
+        File file = new File(nameFile);
+        long size = file.length();
+        byte[] content = new byte[(int) size];
+        FileInputStream fileInputStream = new FileInputStream(file);
+        fileInputStream.read(content);
+        fileInputStream.close();
+        return content;
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,5 @@
 <html>
-<title>$My Servlet$</title>
+<title>My Servlet</title>
 <body>
 <h2>My First Servlet</h2>
 </body>
