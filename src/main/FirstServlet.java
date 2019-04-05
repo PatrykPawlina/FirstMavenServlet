e599214bcf3400e567e6745c894d9e56cd4e448f
Patryk077 <p.pawlina@poczta.onet.pl>
2019-04-05 10:56:40 +0200

    Load name

--- src/main/FirstServlet.java
+++ src/main/FirstServlet.java
@@ -5,7 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.OutputStream;
+import java.io.PrintWriter;
 
 public class FirstServlet extends HttpServlet {
     @Override
@@ -13,22 +13,37 @@ public class FirstServlet extends HttpServlet {
 //        resp.setContentType("text/plain; charset=utf-8");
 //        PrintWriter printWriter = resp.getWriter();
 //        printWriter.println("Hello");
-//        resp.setContentType("text/html; charset=utf-8");
-//        PrintWriter printWriter = resp.getWriter();
-//        printWriter.println("<html><head><title>My Servlet</title></head>");
-//        printWriter.println("<body>");
-//        printWriter.println("<p><h2>This is simple</h></p>");
-//        printWriter.println("<p><h3>Servlet example.</h3></p>");
-//        printWriter.println("</body></html>");
 
-        byte[] buffer = loadFile("C:\\java.png");
-        resp.setContentType("image/jpg");
-        resp.setContentLength(buffer.length);
-        resp.addHeader("Content-Description", "attachment;filename=image.jpg");
-        OutputStream outputStream = resp.getOutputStream();
-        outputStream.write(buffer);
-        outputStream.flush();
+//        Example example of reading text
+        resp.setContentType("text/html; charset=utf-8");
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.println("<html><head><title>My Servlet</title></head>");
+        printWriter.println("<body>");
+        printWriter.println("<p><h2>This is simple</h></p>");
+        printWriter.println("<p><h3>Servlet example.</h3></p>");
+        printWriter.println("<form method=\"post\">");
+        printWriter.println("<p>Give Name:<p><input name=\"name\"/><input type=\"submit\" value=\"OK\"/>");
+        printWriter.println("</form>");
+        printWriter.println("</body></html>");
     }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset=utf-8");
+        PrintWriter printWriter = resp.getWriter();
+        String name = req.getParameter("name");
+        if (name != null && !"".equals(name)) ;
+        printWriter.println("Witaj, " + name);
+    }
+
+//      Example of loading a binary file, e.g. a photo
+//        byte[] buffer = loadFile("C:\\Users\\ppawl\\Pictures\\Honor 7\\IMG_20160914_105018.jpg");
+//        resp.setContentType("image/jpg");
+//        resp.setContentLength(buffer.length);
+//        resp.addHeader("Content-Description", "attachment;filename=image.jpg");
+//        OutputStream outputStream = resp.getOutputStream();
+//        outputStream.write(buffer);
+//        outputStream.flush();
 
     private byte[] loadFile(String nameFile) throws IOException {
         File file = new File(nameFile);
