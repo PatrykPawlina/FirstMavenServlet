a576bc98fd7b3823fe26b7efc684ed6b04572094
Patryk077 <p.pawlina@poczta.onet.pl>
2019-04-14 23:48:24 +0200

    Add login config in web.xml file

--- src/main/servlet/ContextServlet.java
+++ src/main/servlet/ContextServlet.java
@@ -1,18 +1,18 @@
-//package servlet;
-//
-//import javax.servlet.ServletContext;
-//import javax.servlet.ServletException;
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//import java.io.PrintWriter;
-//
-//public class ContextServlet extends HttpServlet {
-//    @Override
-//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        resp.setContentType("text/html; charset=utf-8");
-//        PrintWriter printWriter = resp.getWriter();
-//        ServletContext context = this.getServletContext();
-//    }
-//}
+package servlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ContextServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset=utf-8");
+        PrintWriter printWriter = resp.getWriter();
+        ServletContext context = this.getServletContext();
+    }
+}
